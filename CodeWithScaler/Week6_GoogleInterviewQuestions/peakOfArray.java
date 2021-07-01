dc0ab0e6c626570ecdf846555a24692962c62588
vxhl <moharibishal@gmail.com>
2021-07-01 13:42:54 +0530

    Pushing in PeakOfArray

---
+++ CodeWithScaler/Week6_GoogleInterviewQuestions/peakOfArray.java
@@ -0,0 +1,67 @@
+/*
+Problem Statement:
+Given an array of integers A, find and return the peak element in it.
+An array element is peak if it is not smaller than it's neighbours
+
+Example:
+1. Input: A = [4,17,100,11] Output: 100
+2. Input: A = [4,18,17,100,11] Output: 18
+*/
+
+package CodeWithScaler.Week6_GoogleInterviewQuestions;
+
+public class peakOfArray {
+
+    // Approach 1: Iterative O(N) approach
+    // Time Complexity: O(N)
+    static int peakElement(int[] arr)
+    {
+        int n = arr.length;
+        if(arr[0]>arr[1]) return arr[0];
+        else if(arr[n-1]>arr[n-2]) return arr[n-1];
+        for(int i = 1; i< n-1; i++)
+        {
+            if(arr[i]>arr[i+1] && arr[i]>arr[i-1])
+            {
+                return arr[i];
+            } 
+
+        }
+        return 0;
+    }
+
+    // Approach 2: Binary Search approach
+    // Time Complexity: O(logN)
+    static int peakElementOptimised(int[] A)
+    {
+        int n = A.length;
+        int l = 0;
+        int r = n-1;
+        int mid = l+r/2;
+        if(n==1) return A[0];
+        while(l<=r)
+        {
+            if((mid == 0 || A[mid-1]<=A[mid]) && (mid==n-1 || A[mid+1]<=A[mid]))
+            {
+                return A[mid];
+            }
+            if(mid-1>=0 && A[mid-1]>A[mid]) 
+            {
+                mid = mid-1;
+            }
+            else if(mid+1>=0 && A[mid+1]>A[mid])
+            {
+                mid = mid+1;
+            }
+            l++;
+            r--;
+        }
+        return A[mid];
+    }
+    public static void main(String[] args) {
+        int arr[] = {5,18,17,100,11};
+        System.out.println("The peak element is: " + peakElement(arr));
+        System.out.println("The peak element is: " + peakElementOptimised(arr));
+    }
+
+}
