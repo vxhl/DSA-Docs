d2fb9cc0c4c70d5d4da8d684acd4026384b81feb
vxhl <moharibishal@gmail.com>
2021-07-13 17:24:30 +0530

    Update ReverseLinkedList.java

--- LinkedLists/ReverseLinkedList.java
+++ LinkedLists/ReverseLinkedList.java
@@ -13,22 +13,45 @@ class ReverseTheLinkedList
             next = null;
         }
     }
-    NODE reverse(NODE node)
+    // Approach 1: Reversing LinkedList using Iterative method
+    NODE ReverseIterativeLinkedList(NODE node)
     {
+        // First let us define the previous node for the head node
         NODE prev = null;
+        // We assign our head node to current
         NODE current = node;
+        // We then assign a new node next for storing the link for the next nodes
         NODE next = null;
         while(current!=null)
-        {
-            // We first store the next node
+        {   
+            // As said before we store the address for the next node in the `next` node
             next = current.next;
+            // Then we assign the pointer for the next node of current node to the previous node
             current.next = prev; 
+            // We then change the value of prev for the operation with the next node
             prev = current;
+            // We also get the address for the next node
             current = next;
         }
+        // At the end we assign our head node as prev and completing the reverse operation
         node = prev;
         return node;
     }
+    // Approach 2: Reversing linkedlist using recurssive method.
+    static void ReverseRecursiveLinkedList(NODE node)
+    {
+        if(node.next == null)
+        {
+            head = node;
+            return;
+        }
+        ReverseRecursiveLinkedList(node.next);
+        NODE q = node.next;
+        q.next = node;
+        node.next = null;
+    }
+
+
     void printList(NODE node)
     {
         while (node != null) {
@@ -45,10 +68,15 @@ class ReverseTheLinkedList
  
         System.out.println("Given Linked list");
         list.printList(head);
-        head = list.reverse(head);
+        head = list.ReverseIterativeLinkedList(head);
         System.out.println("");
-        System.out.println("Reversed linked list ");
+        System.out.print("Reversed linked list using Iterative approach ");
         list.printList(head);
+        ReverseRecursiveLinkedList(head);
+        
+        System.out.print("Reversed linked list using Recurssive approach ");
+        list.printList(head);
+
     }
 
 }
