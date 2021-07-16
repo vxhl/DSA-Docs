3b843b11789b42bfe8969bdf7b976c0f6f975a74
vxhl <moharibishal@gmail.com>
2021-07-16 19:16:07 +0530

    Pushing Reverse Linked list K groups

---
+++ LinkedLists/DetectLoopInLinkedList.java
@@ -0,0 +1,21 @@
+/*
+Problem Statement:
+Given a linkedlist of N nodes. Check if it has a loop
+Input:
+N = 3
+value[] = {1,3,4}
+x = 2
+Output: True
+Explanation: In above test case N = 3.
+The linked list with nodes N = 3 is
+given. Then value of x=2 is given which
+means last node is connected with xth
+node of linked list. Therefore, there
+exists a loop.
+*/
+
+package LinkedLists;
+
+public class DetectLoopInLinkedList {
+    
+}
--- LinkedLists/ReverseLinkedListKGroups.java
+++ LinkedLists/ReverseLinkedListKGroups.java
@@ -1,3 +1,16 @@
+/*
+Problem Statement: 
+Given a linked list of size N. The task is to reverse 
+every k nodes (where k is an input to the function) in the linked list.
+Input:
+LinkedList: 1->2->2->4->5->6->7->8
+K = 4
+Output: 4 2 2 1 8 7 6 5 
+Explanation: 
+The first 4 elements 1,2,2,4 are reversed first 
+and then the next 4 elements 5,6,7,8. Hence, the 
+resultant linked list is 4->2->2->1->8->7->6->5.
+*/
 package LinkedLists;
 
 public class ReverseLinkedListKGroups {
