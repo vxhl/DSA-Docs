44cdb0ea5e7198e62bfb8293b164a972969c6aac
vxhl <moharibishal@gmail.com>
2021-08-19 23:45:33 +0530

    push

---
+++ JavaStuff/CollectionFramework.java
@@ -0,0 +1,14 @@
+package JavaStuff;
+import java.util.*;
+
+public class CollectionFramework {
+    // 1. ArrayList
+    // Their size can be changed dynamically
+    public static void main(String[] args) {
+     
+    ArrayList<String> studentsName = new ArrayList<>();
+    studentsName.add("Rakesh");
+     
+}
+
+}
