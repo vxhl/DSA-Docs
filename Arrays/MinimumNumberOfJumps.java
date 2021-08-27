94021b29fffa588f4e9708f69801c502a42e2ddc
vxhl <moharibishal@gmail.com>
2021-08-27 23:13:00 +0530

    Update MinimumNumberOfJumps.java

--- Arrays/MinimumNumberOfJumps.java
+++ Arrays/MinimumNumberOfJumps.java
@@ -15,26 +15,53 @@ package Arrays;
 import java.util.*;
 
 public class MinimumNumberOfJumps {
+
+
+    // Approach 1: Naive Recursive Approach 
+    // Time Complexity: O(N^2)
+    static int minjumps(int arr[], int l, int h)
+    {
+        // Base Case: When source and destination are same
+        if(h==l) return 0;
+        
+        // When nothing is reachable from the given source
+        if(arr[l]==0) return -1;
+
+        // Traverse through all the points reachable from arr[l].
+        // Recursively get the minimum number of jumps needed to reach arr[h]
+        // from these reachable points.
+        int min = Integer.MAX_VALUE;
+        for(int i=l+1; i<=h && i<=l+arr[l]; i++)
+        {
+            int jumps = minjumps(arr, i, h);
+            if(jumps != Integer.MAX_VALUE && jumps + 1< min)
+            {
+                min = jumps+1;
+            }
+        }
+        return min;
+    }
     public static void main(String[] args) {
         int[] arr = {2, 3, 1, 1, 2, 4, 2, 0, 1, 1};
-        int count = 0;
-        int jumps = 0;
-        int n = arr.length;
-        for(int i=0; i<n; i++)
-        {
-            jumps = arr[i];
-            if(jumps==0)
-            {
-                System.out.print(0);
-                break;
-            }
-            i+=jumps;
-            if(i>n)
-            {
-                i=n;
-            }
-            count+=1;
-        }
-        System.out.print("The minimum number of jumps are: "+ count);
+
+        // int count = 0;
+        // int jumps = 0;
+        // int n = arr.length;
+        // for(int i=0; i<n; i++)
+        // {
+        //     jumps = arr[i];
+        //     if(jumps==0)
+        //     {
+        //         System.out.print(0);
+        //         break;
+        //     }
+        //     i+=jumps;
+        //     if(i>n)
+        //     {
+        //         i=n;
+        //     }
+        //     count+=1;
+        // }
+        // System.out.print("The minimum number of jumps are: "+ count);
     }
 }
