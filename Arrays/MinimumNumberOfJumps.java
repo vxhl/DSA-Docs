d92f9c9402eee83aa25f2d2dc0a2b46aa778994f
vxhl <moharibishal@gmail.com>
2021-08-29 11:12:13 +0530

    ushin in minJumps array problem

--- Arrays/MinimumNumberOfJumps.java
+++ Arrays/MinimumNumberOfJumps.java
@@ -41,9 +41,38 @@ public class MinimumNumberOfJumps {
         }
         return min;
     }
-    public static void main(String[] args) {
-        int[] arr = {2, 3, 1, 1, 2, 4, 2, 0, 1, 1};
+    // arr[] = { 1, 3, 6, 1, 0, 9 };
+ 
+    static int minJumpsDynamicApproach(int[] arr, int n)
+    {
+        // jumps[n-1] will hold our result
+        int jumps[] = new int[n];
+        int i, j;
 
+        jumps[0] = 0;
+        // Find the minimum number of jumps to reach arr[i]
+        // from arr[0] and assign this value to jumps[i]
+        for(i=1; i<n; i++)
+        {
+            jumps[i] = Integer.MAX_VALUE;
+            for(j=0; j<i; j++)
+            {
+                if(i<=j +arr[j] && jumps[j] != Integer.MAX_VALUE)
+                {
+                    jumps[i] = Math.min(jumps[i], jumps[j]+1);
+                    break;
+                }
+            }
+        }
+        return jumps[n-1];
+
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 6, 1, 0, 9 };
+        int n = arr.length;
+        System.out.println(minJumpsDynamicApproach(arr, n));
         // int count = 0;
         // int jumps = 0;
         // int n = arr.length;
