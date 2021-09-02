2491f4a9f17d3e8679481563c96f06026cef372f
vxhl <moharibishal@gmail.com>
2021-09-02 12:58:16 +0530

    Merge without extra space

---
+++ Arrays/MergeIntervals.java
@@ -0,0 +1,18 @@
+/*
+Problem Statement: 
+Given an array of intervals where 
+intervals[i] = [starti, endi]
+, merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+Examples: 
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+
+
+*/
+package Arrays;
+
+public class MergeIntervals 
+{
+    
+}
