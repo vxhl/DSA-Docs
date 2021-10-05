eb3518567c81be4350adfeb1c87d012afed84bac
vxhl <moharibishal@gmail.com>
2021-10-05 10:44:20 +0530

    Pushing in the climbing stairs problem

---
+++ Matrices/test.java
@@ -0,0 +1,12 @@
+package Matrices;
+
+public class test {
+    public static void main(String[] args)
+        {
+        
+            double d = 100.04;  
+        
+            float f = d;	
+        System.out.println("Float value "+f); 
+        }
+}
