49ed1a70759f9cdd2c0545bda93ffae88c80a034
vxhl <moharibishal@gmail.com>
2022-02-17 18:34:37 +0530

    Floyd's cycle finding algorithm on findingDuplicates287
    This is probably one of the most fascinating problems I have ever seen. How tf did they even come up with this???

---
+++ Striver_SDESHEET/Day2/FindDuplicateNumber_287.java
@@ -0,0 +1,17 @@
+package Striver_SDESHEET.Day2;
+import java.util.*;
+
+// This is the n+1 integers duplicate problem
+
+public class FindDuplicateNumber_287 {
+    
+
+
+    public static void main(String[] args) {
+        int[] arr = {3,1,3,4,2};
+        for(int i =1; i<arr.length-1; i++)
+        {
+            if(arr[i] == )
+        }
+    }
+}
