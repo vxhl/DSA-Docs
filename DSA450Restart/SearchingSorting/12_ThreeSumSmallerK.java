1ea078fdca38bc1069a521bd17c6139ab4530cb2
Bishal Mohari <52225734+vxhl@users.noreply.github.com>
2022-05-21 22:48:38 +0530

    Adding today's only problem

---
+++ DSA450Restart/SearchingSorting/12_ThreeSumSmallerK.java
@@ -0,0 +1,32 @@
+package DSA450Restart.SearchingSorting;
+
+class Solution_ThreeSumSmallerK
+{
+    long countTriplets(long arr[], int n,int target)
+    {
+        // Same as 3 sum probably
+        
+        long ans = 0;
+        Arrays.sort(arr);
+        for(int i=0; i<n-2; i++)
+        {
+            int low = i+1;
+            int high = n-1;
+            
+            while(low<high)
+            {
+                if(arr[i]+arr[low]+arr[high] >= target)
+                {
+                    high--;
+                }
+                // else if(arr[i]+arr[low]+arr[high]<target)
+                else
+                {
+                    ans += high-low;
+                    low++;
+                }
+            }
+        }
+        return ans;
+    }
+}
