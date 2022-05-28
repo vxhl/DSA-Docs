4a8b4b54e6f0d08f3e3c0ff7459b857b7e42c70b
Bishal Mohari <52225734+vxhl@users.noreply.github.com>
2022-05-28 10:05:44 +0530

    adding optimisied approach for ProductExceptSelf

--- DSA450Restart/SearchingSorting/15_ProductArrayPuzzle.java
+++ DSA450Restart/SearchingSorting/15_ProductArrayPuzzle.java
@@ -2,35 +2,81 @@ import java.util.*;
 
 class Solution_Puzzles
 {
-    public static int[] puzzles(int[] arr)
-    {
-        int[] res = new int[arr.length];
 
-        for(int i=0; i<arr.length; i++){
-            int iter = 0; // So this will loop over our entire array
-            int prod = 1; // this is what we need in our result array
-            while(iter!=arr.length){
-                
-                // So we multiply prod as long as it is not ith
-                if(iter!=i)
+    
+        //     public int[] productExceptSelf(int[] arr) {
+        //         int[] res = new int[arr.length];
+        
+        //         for(int i=0; i<arr.length; i++){
+        //             int iter = 0; // So this will loop over our entire array
+        //             int prod = 1; // this is what we need in our result array
+        //             while(iter!=arr.length){
+                        
+        //                 // So we multiply prod as long as it is not ith
+        //                 if(iter!=i)
+        //                 {
+        //                     // 3*5*6*2
+        //                     prod = prod*arr[iter];
+        //                 }
+        //                 iter++;
+        //             }
+        //             // We store the prod in our res[i];
+        //             // Then we move on to the next element
+        //             res[i] = prod;
+        //         }
+        
+        //         return res;
+        //     }
+            
+            public static int[] productExceptSelf(int[] arr) {
+                int[] res = new int[arr.length];
+                // We simply use the prefix sum thing but how?
+                int[] left = new int[arr.length];
+                int[] right = new int[arr.length];
+                left[0] = arr[0];
+                right[arr.length-1] = arr[arr.length-1]; 
+                for(int i=1; i<arr.length; i++)
                 {
-                    // 3*5*6*2
-                    prod = prod*arr[iter];
+                    left[i] = arr[i]*left[i-1]; 
+                    System.out.print(left[i]+" ");
                 }
-                iter++;
+                 
+                    System.out.println();
+                for(int i=arr.length-2; i>=0; i--)
+                {
+                    right[i] = arr[i]*right[i+1];
+                     
+                    System.out.print(right[i]+" ");
+                }
+                
+                for(int i=0; i<arr.length; i++)
+                {
+                    // We have corner case for the first and the last element right
+                    if(i==0)
+                    {
+                        res[i] = right[i+1];
+                    }
+                    else if(i==arr.length-1)
+                    {
+                        res[i] = left[i-1];
+                    }
+                    
+                    else
+                    {
+                        res[i] = left[i-1]*right[i+1];
+                    }
+                }
+                return res;
+                
+                
+            
             }
-            // We store the prod in our res[i];
-            // Then we move on to the next element
-            res[i] = prod;
-        }
-
-        return res;
-    }
     public static void main(String[] args) {
         int[] arr = {10,3,5,6,2};
-        for(int i: puzzles(arr))
+        for(int i: productExceptSelf(arr))
         {
             System.out.print(i+ " ");    
         }
+
     }
 }
