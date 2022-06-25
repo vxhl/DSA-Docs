0b3dd0948971639c1af6d14dfd34ac17ad8a567a
Bishal Mohari <52225734+vxhl@users.noreply.github.com>
2022-06-25 23:23:48 +0530

    Adding the problems

--- DSA450Restart/DynamicProgramming_Recursion/337_HouseRobber3.java
+++ DSA450Restart/DynamicProgramming_Recursion/337_HouseRobber3.java
@@ -13,6 +13,17 @@
  *     }
  * }
  */
+
+class TreeNode
+{
+    TreeNode left;
+    TreeNode right;
+    int val;
+    public TreeNode(int val)
+    {
+        this.val = val;
+    }
+}
 class Solution {
 
 
@@ -66,6 +77,32 @@ class Solution {
         mp.put(root,Math.max(include, exclude));
         
         return Math.max(include, exclude);
+    }
+
+
+    public int rob_dp(TreeNode root) {
+        int[] ans = helper_dp(root);
+        return Math.max(ans[0], ans[1]);
+    }
+    
+    private int[] helper_dp(TreeNode root)
+    {
+        if(root == null) return new int[2];
+        int[] left = helper_dp(root.left);
+        int[] right = helper_dp(root.right);
+        int[] res = new int[2];
+        // Our first element of the 2 element array holds the cases when we are not picking the root value and hence
+        // We are adding up the maximum value that we garner from the left and right subtrees
+        res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        
+        
+        // Our second element holds the case where we are picking our root value, and since we are picking the root value we 
+        // add it up with the cases when we are not picking our directly left and right nodes
+        res[1] = root.val + left[0] + right[0];
+        // Here left[0] signifies all the cases where we do not pick the adjacent root to our left subtree
+        // right[0] represents all the cases where we do not pick the root value or the adjacent root to our right subtree
+        
+        return res;
         
         
     }
