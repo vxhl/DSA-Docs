f8a817234ed4c03423c4148a8947b295a761a546
Bishal Mohari <52225734+vxhl@users.noreply.github.com>
2022-07-23 23:48:10 +0530

    Boundary Binary Tree 3 DFS

--- DSA450Restart/Trees/BoundaryBinaryTree.java
+++ DSA450Restart/Trees/BoundaryBinaryTree.java
@@ -1,4 +1,69 @@
-public class Solution {
+class Solution1 {
+    /**
+     * @param root: a TreeNode
+     * @return: a list of integer
+     */
+    List<Integer> ans = new ArrayList<>();
+    public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+        // write your code here
+        if(root == null) return ans;
+        ans.add(root.val);
+        if(root.left == null && root.right==null) return ans;
+
+        dfsLeft(root.left);
+        dfsLeaf(root);
+        dfsRight(root.right);
+        return ans;
+
+            
+    }
+    private void dfsLeaf(TreeNode root)
+    {
+        if(root == null) return;
+
+        if(root.left == null && root.right == null)
+        {
+            ans.add(root.val);
+            return;
+        }
+        dfsLeaf(root.left);
+        dfsLeaf(root.right);
+    }
+    private void dfsLeft(TreeNode root)
+    {
+        if(root == null || (root.left == null && root.right == null)) return;
+        
+        ans.add(root.val);
+        if(root.left!=null)
+        {
+            dfsLeft(root.left);
+        }
+        else
+        {
+            dfsLeft(root.right);
+        }
+    }
+
+    private void dfsRight(TreeNode root)
+    {
+        
+        if(root == null || (root.left == null && root.right == null)) return;
+        
+        if(root.right!=null)
+        {
+            dfsRight(root.right);
+        }
+        else
+        {
+            dfsRight(root.left);
+        }
+        ans.add(root.val);
+    }
+}
+
+
+
+class Solution2 {
     /**
      * @param root: a TreeNode
      * @return: return a list of integer
