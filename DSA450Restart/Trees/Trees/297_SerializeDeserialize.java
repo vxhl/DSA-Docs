6f8a6ccfe903e0983ffe25c54f4feb512f329e26
Bishal Mohari <52225734+vxhl@users.noreply.github.com>
2022-07-24 16:25:55 +0530

    BFS for serial and deserial

--- DSA450Restart/Trees/Trees/297_SerializeDeserialize.java
+++ DSA450Restart/Trees/Trees/297_SerializeDeserialize.java
@@ -68,3 +68,80 @@ class Codec {
 // Codec ser = new Codec();
 // Codec deser = new Codec();
 // TreeNode ans = deser.deserialize(ser.serialize(root));
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+// BFS Approach
+class Codec2 {
+    // Now for the BFS approach
+    // Encodes a tree to a single string.
+    static String nulls = "n";
+    public String serialize(TreeNode root) {
+        if(root == null) return "";
+        // We are serializing the current tree into a String
+        Deque<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        StringBuilder sb = new StringBuilder();
+        
+        while(!q.isEmpty())
+        {
+            TreeNode curr = q.poll();
+            if(curr == null)
+            {
+                sb.append("n ");
+            }
+            else
+            {
+            sb.append(curr.val + " ");
+            q.offer(curr.left);
+            q.offer(curr.right);
+            }
+        }
+        
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if(data == "") return null;
+        String[] values = data.split(" ");
+        TreeNode root = new TreeNode(Integer.parseInt(values[0]));
+        Deque<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        for(int i=1; i<values.length; i++)
+        {
+            TreeNode par = q.poll();
+            
+            if(!values[i].equals("n"))
+            {
+                TreeNode left = new TreeNode(Integer.parseInt(values[i]));
+                par.left = left;
+                q.add(left);
+            }
+            // We increment i here since we want to check both the left and right nodes for the current node
+            i++;
+            if(!values[i].equals("n"))
+            {
+                TreeNode right = new TreeNode(Integer.parseInt(values[i]));
+                par.right = right;
+                q.add(right);
+            }
+        }
+        return root;
+        
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
