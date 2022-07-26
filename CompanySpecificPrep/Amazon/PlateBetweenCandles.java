e05b090b626c7f9218cb6b01b77f5ade4072e3eb
Bishal Mohari <52225734+vxhl@users.noreply.github.com>
2022-07-26 10:52:29 +0530

    Adding optimised approach for platesBetweenCandles

--- CompanySpecificPrep/Amazon/PlateBetweenCandles.java
+++ CompanySpecificPrep/Amazon/PlateBetweenCandles.java
@@ -1,6 +1,60 @@
 class Solution {
     // O(N+M) approach
     // O(N*(MLogM)) approach
+
+    public int[] platesBetweenCandles1(String s, int[][] queries) {
+        
+        int n = s.length();
+        
+        int[] leftMostCandles = new int[n];
+        int[] rightMostCandles = new int[n];
+        int[] platesPrefix = new int[n];
+        
+        // We compute all the indexes for the leftMost prefix for candles
+        int index = -1;
+        for(int i=0; i<n; i++){
+            if(s.charAt(i) == '|'){
+                index = i;
+            }
+            leftMostCandles[i] = index;
+        }
+        index = -1;
+        for(int i=n-1; i>=0; i--){
+            if(s.charAt(i) == '|'){
+                index = i;
+            }
+            rightMostCandles[i] = index;
+        }
+        int currPlates = 0;
+        for(int i=0; i<n; i++){
+            if(s.charAt(i)=='*'){
+                currPlates++;
+            }
+            platesPrefix[i] = currPlates;
+        }
+        int[] ans = new int[queries.length];
+        
+        for(int i=0; i<queries.length; i++){
+            int left = rightMostCandles[queries[i][0]];
+            int right = leftMostCandles[queries[i][1]];
+            // System.out.print(left + " "+ right);
+            if(left == -1 || right == -1){
+                ans[i] = 0;
+            }
+            else{
+                if(right-left>0){
+                    ans[i] = platesPrefix[right]-platesPrefix[left];
+                }
+                else{
+                    ans[i] = 0;
+                }
+            }
+        }
+        
+        return ans;
+    }
+
+
     public int[] platesBetweenCandles(String s, int[][] queries) {
         
         int[] prefixPlates = new int[s.length()];
