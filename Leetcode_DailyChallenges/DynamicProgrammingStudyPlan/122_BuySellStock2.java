a7f7733192f03eb48a5afa90838e5cfbd26022b2
Bishal Mohari <52225734+vxhl@users.noreply.github.com>
2022-08-17 09:57:19 +0530

    Adding new approach for buy and sell stock 2

--- Leetcode_DailyChallenges/DynamicProgrammingStudyPlan/122_BuySellStock2.java
+++ Leetcode_DailyChallenges/DynamicProgrammingStudyPlan/122_BuySellStock2.java
@@ -1,4 +1,31 @@
 class Solution {
+
+        // Recursion with memoisation 
+        public int maxProfitDP(int[] prices) {
+            int[][] dp = new int[prices.length][2];
+            for(int[]d: dp) Arrays.fill(d,-1);
+            return helper(0, prices, 1,dp);
+        }
+        
+        private int helper(int i,int[] prices, int buy,int[][]dp){
+            if(i==prices.length){
+                return 0;
+            }
+            if(dp[i][buy]!=-1){
+                return dp[i][buy];
+            }
+            // If we are allowed to buy
+            if(buy==1){
+                // Then we set the condition for buying or selling for that day and get the max of the two
+                return dp[i][buy]=Math.max(-prices[i]+helper(i+1,prices, 0,dp), helper(i+1,prices, 1,dp));
+            }
+            // If we are not allowed to buy then we sell ofc
+            else{
+                return dp[i][buy]=Math.max(prices[i]+helper(i+1,prices, 1,dp), 0+helper(i+1, prices,0,dp));
+            }
+    }
+
+    // Constant space linear time solution
     public int maxProfit(int[] prices) {
         // Ok got a very awesome solution, this is gonna stay hopefully
         int buy = 0;
